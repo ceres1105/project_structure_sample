12357c77ccbfaa0895657c5e3351a43e6f7362c1
ceres1105 <ceres1105@gmail.com>
2020-07-08 15:31:47 +0900

    .

--- src/component/main/MainContainer.js
+++ src/component/main/MainContainer.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import Login from "../../screen/Login";
 
 const MainContainer = () => {
   return (
